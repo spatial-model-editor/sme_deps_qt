c817fe5f70cdb3f2ba26c81e9a0f47bc940dc44a
Liam Keegan <liam@keegan.ch>
2021-05-31 14:30:51 +0200

    rename qt5 -> qt & refactor build script

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# sme_deps_qt5 [![Release Builds](https://github.com/spatial-model-editor/sme_deps_qt5/actions/workflows/release.yml/badge.svg)](https://github.com/spatial-model-editor/sme_deps_qt5/actions/workflows/release.yml)
+# sme_deps_qt [![Release Builds](https://github.com/spatial-model-editor/sme_deps_qt/actions/workflows/release.yml/badge.svg)](https://github.com/spatial-model-editor/sme_deps_qt/actions/workflows/release.yml)
 
-A minimal static build of Qt6:
+A minimal static build of Qt:
 
 - Qt version: [Qt 6.1.0](https://doc.qt.io/qt-6/)
 - Qt license: [LGPLv3](https://doc.qt.io/qt-6/lgpl.html)
 
 Get the latest versions here:
 
-- linux (gcc 9 / Ubuntu 18.04): [sme_deps_qt5_linux.tgz](https://github.com/spatial-model-editor/sme_deps_qt5/releases/latest/download/sme_deps_qt5_linux.tgz)
-- osx (Apple clang 12 / macOS 10.15): [sme_deps_qt5_osx.tgz](https://github.com/spatial-model-editor/sme_deps_qt5/releases/latest/download/sme_deps_qt5_osx.tgz)
-- win32 (mingw-w64-i686-gcc 10): [sme_deps_qt5_win32.tgz](https://github.com/spatial-model-editor/sme_deps_qt5/releases/latest/download/sme_deps_qt5_win32.tgz)
-- win64 (mingw-w64-x86_64-gcc 10): [sme_deps_qt5_win64.tgz](https://github.com/spatial-model-editor/sme_deps_qt5/releases/latest/download/sme_deps_qt5_win64.tgz)
+- linux (gcc 9 / Ubuntu 18.04): [sme_deps_qt_linux.tgz](https://github.com/spatial-model-editor/sme_deps_qt/releases/latest/download/sme_deps_qt_linux.tgz)
+- osx (Apple clang 12 / macOS 10.15): [sme_deps_qt_osx.tgz](https://github.com/spatial-model-editor/sme_deps_qt/releases/latest/download/sme_deps_qt_osx.tgz)
+- win32 (mingw-w64-i686-gcc 10): [sme_deps_qt_win32.tgz](https://github.com/spatial-model-editor/sme_deps_qt/releases/latest/download/sme_deps_qt_win32.tgz)
+- win64 (mingw-w64-x86_64-gcc 10): [sme_deps_qt_win64.tgz](https://github.com/spatial-model-editor/sme_deps_qt/releases/latest/download/sme_deps_qt_win64.tgz)
 
 See <https://github.com/spatial-model-editor/spatial-model-editor> for an example of use
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-echo "QT5_VERSION = $QT5_VERSION"
+echo "QT_VERSION = $QT_VERSION"
 echo "INSTALL_PREFIX = $INSTALL_PREFIX"
 echo "SUDO_CMD = $SUDO_CMD"
 echo "CONFIGURE_EXTRAS = $CONFIGURE_EXTRAS"
@@ -14,10 +14,10 @@ g++ --version
 mkdir qt
 cd qt
 
-# download Qt5 sources to qt/qt5
+# download Qt sources to qt/qt5
 git clone https://code.qt.io/qt/qt5.git
 cd qt5
-git checkout $QT5_VERSION
+git checkout $QT_VERSION
 # only need qtbase submodule
 git submodule update --init qtbase
 cd ..
@@ -25,7 +25,30 @@ cd ..
 # make build dir in qt/build and run cmake
 mkdir build
 cd build
-cmake ../qt5/qtbase -G "Ninja" -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DQT_USE_BUNDLED_BundledFreetype=ON -DQT_USE_BUNDLED_BundledHarfbuzz=ON -DQT_USE_BUNDLED_BundledLibpng=ON -DQT_USE_BUNDLED_BundledPcre2=ON -DFEATURE_system_doubleconversion=OFF -DFEATURE_system_harfbuzz=OFF -DFEATURE_system_jpeg=OFF -DFEATURE_system_libb2=OFF -DFEATURE_system_pcre2=OFF -DFEATURE_system_png=OFF -DFEATURE_system_proxies=OFF -DFEATURE_system_sqlite=OFF -DFEATURE_system_textmarkdownreader=OFF -DFEATURE_system_zlib=OFF -DFEATURE_zstd=OFF -DFEATURE_openssl=OFF -DFEATURE_sql=OFF -DFEATURE_icu=OFF -DFEATURE_testlib=ON -DBUILD_WITH_PCH=OFF ${CONFIGURE_EXTRAS}
+cmake ../qt5/qtbase -G "Ninja" \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
+  -DQT_USE_BUNDLED_BundledFreetype=ON \
+  -DQT_USE_BUNDLED_BundledHarfbuzz=ON \
+  -DQT_USE_BUNDLED_BundledLibpng=ON \
+  -DQT_USE_BUNDLED_BundledPcre2=ON \
+  -DFEATURE_system_doubleconversion=OFF \
+  -DFEATURE_system_harfbuzz=OFF \
+  -DFEATURE_system_jpeg=OFF \
+  -DFEATURE_system_libb2=OFF \
+  -DFEATURE_system_pcre2=OFF \
+  -DFEATURE_system_png=OFF \
+  -DFEATURE_system_proxies=OFF \
+  -DFEATURE_system_textmarkdownreader=OFF \
+  -DFEATURE_system_zlib=OFF \
+  -DFEATURE_zstd=OFF \
+  -DFEATURE_openssl=OFF \
+  -DFEATURE_sql=OFF \
+  -DFEATURE_icu=OFF \
+  -DFEATURE_testlib=ON \
+  -DBUILD_WITH_PCH=OFF \
+  ${CONFIGURE_EXTRAS}
 
 time ninja
 $SUDO_CMD ninja install
@@ -37,4 +60,4 @@ $INSTALL_PREFIX/bin/qmake -v
 # make tarball of installation
 mkdir artefacts
 cd artefacts
-tar -zcvf sme_deps_qt5_${OS}.tgz ${INSTALL_PREFIX}/*
+tar -zcvf sme_deps_qt_${OS}.tgz ${INSTALL_PREFIX}/*
